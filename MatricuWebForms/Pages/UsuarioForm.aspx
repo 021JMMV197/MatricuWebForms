4d672c7c2d4206f87c380dc341c621617cdff628
Juan Mejia <elote021197@gmail.com>
2024-10-07 21:43:19 -0600

    Conexion a la Base de Datos Creacion de Login Aplicacion de Estilos Obtencion de Data

---
+++ MatricuWebForms/Pages/UsuarioForm.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UsuarioForm.aspx.cs" Inherits="MatricuWebForms.Pages.UsuarioForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login de Usuario</title>
+    <link rel="stylesheet" type="text/css" href="../Styles/StyleSheet.css"
+</head>
+<body>
+    <form id="loginform" runat="server" method="post">
+        <h2>Login de Usuario</h2>
+        <label>Nickname:</label>
+        <input type="text" id="nickname" name="nickname" class="input" required/> <br /> 
+
+        <label>Contraseña:</label>
+        <input type="password" id="contrasenna" name="contrasenna" class="input" required/> <br /> 
+
+        <input type="submit" value="Login" class="button"/>
+
+    </form>
+</body>
+</html>
---
+++ MatricuWebForms/Pages/UsuarioForm.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MatricuWebForms.Pages
+{
+    public partial class UsuarioForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string nick = Request.Form["nickname"];
+            string clave = Request.Form["contrasenna"];
+
+            using (SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString))
+            {
+                conn.Open(); //abrimos conexion 
+                String query = "select * from Usuarios where NICKNAME = @nick and CONTRASENNA = @clave";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@nick", nick);
+                cmd.Parameters.AddWithValue("@clave", clave);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("Ingreso Exitoso!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Intento fallido!");
+                }
+
+            }
+        }
+    }
+}
